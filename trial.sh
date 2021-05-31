149f23bb4bde89c67ae4a6dd5cb1e517ce22f588
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-05-31 15:40:51 +0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -44,22 +44,33 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Informasi Trial SSH & OpenVPN"
+echo -e "Thank You For Using Our Services"
+echo -e "SSH & OpenVPN Account Info"
 echo -e "Username       : $Login "
 echo -e "Password       : $Pass"
 echo -e "===============================" | lolcat
+echo -e "Domain         : ${domain}"
 echo -e "Host           : $IP"
-echo -e "CITY           : $CITY"
 echo -e "ISP            : $ISP"
+echo -e "CITY           : $CITY"
 echo -e "OpenSSH        : 22"
 echo -e "Dropbear       : 109, 143"
-echo -e "SSL/TLS        :$ssl"
-echo -e "Port Squid     :$sqd"
+echo -e "SSL/TLS        : $ssl"
+echo -e "Port SSH CDN   : 2082"
+echo -e "Port Squid     : $sqd"
 echo -e "OpenVPN        : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
 echo -e "OpenVPN        : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
 echo -e "OpenVPN        : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
 echo -e "badvpn         : 7100-7300"
 echo -e "===============================" | lolcat
-echo -e "Aktif Sampai   : $exp"
+echo -e "PAYLOAD"
+echo -e "=========" | lolcat
+echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "=================" | lolcat
+echo -e "SETING HOST SSH"
+echo -e "=================" | lolcat
+echo -e "bimbel.ruangguru.com:2082@$Login:$Pass"
+echo -e "================================" | lolcat
+echo -e "Expired On     : $exp"
 echo -e "AutoScript By Endka"
 
