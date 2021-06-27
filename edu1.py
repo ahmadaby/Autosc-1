27527577c5844aeb5b5a5ccaada9dac0897b7cb3
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-06-27 23:19:39 +0700

    Update edu1.py

--- edu1.py
+++ edu1.py
@@ -11,7 +11,7 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:777'
+DEFAULT_HOST = '127.0.0.1:1194'
 RESPONSE = 'HTTP/1.1 101 Endka-Stores\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
