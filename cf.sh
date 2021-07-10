b8c9e85ce652b249c7df783555e5f22f62b25425
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-07-11 00:08:37 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -13,9 +13,9 @@ echo "Only For Premium Users"
 exit 0
 fi
 apt install jq curl -y
-DOMAIN=vmess.my.id
+DOMAIN=end-ka.tech
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.vmess.my.id
+SUB_DOMAIN=${sub}.end-ka.tech
 CF_ID=pradiasangindraswari@gmail.com
 CF_KEY=75b54a5c9fd50751003abac0fefee534528c0
 set -euo pipefail
