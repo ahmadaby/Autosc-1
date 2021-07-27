3cec4acd8bab7645a83e1cfb2c2479596bf387dd
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-07-27 19:43:35 +0700

    Update trgoo.sh

--- trgoo.sh
+++ trgoo.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
-# Wiki: https://github.com/p4gefau1t/trojan-go
-
-domain=$(cat /root/domain)
-
-#install bbr
-wget https://raw.githubusercontent.com/Endka22/Autosc/main/bbr.sh &&  chmod +x bbr.sh && ./bbr.sh
-
-# Install Trojan-GO
-mkdir -p "/etc/trojan-go/"
-wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.4.2/trojan-go-linux-amd64.zip
-unzip trojan-go-linux-amd64.zip
-chmod +x /usr/local/bin/trojan-go
+#!/bin/sh
+latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+echo "The latest version of trojan-go is ${latest_version}"
+trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/v${latest_version}/trojan-go-linux-amd64.zip"
+mkdir -p "/usr/bin/trojan-go"
+mkdir -p "/etc/trojan-go"
+cd `mktemp -d`
+curl -sL "${trojango_link}" -o trojan-go.zip
+unzip -q trojan-go.zip && rm -rf trojan-go.zip
+mv trojan-go /usr/bin/trojan-go/trojan-go
+mv geoip.dat /etc/trojan-go/geoip.dat
+mv geosite.dat /etc/trojan-go/geosite.dat
+mv example/trojan-go.service /etc/systemd/system/trojan-go.service
+if [ ! -f "/etc/trojan-go/config.json" ]; then
+  mv example/sever.json /etc/trojan-go/config.json
+fi
+systemctl daemon-reload
+systemctl reset-failed
+echo -e "Trojan-go has been installed.\n"
+echo -e "Trojan-go\t-> /usr/bin/trojan-go/trojan-go"
+echo -e "config.json\t-> /etc/trojan-go/config.json"
 touch /etc/trojan-go/akun.conf
 touch /etc/trojan-go/trojan-go.pid
 mkdir /var/log/trojan-go/
