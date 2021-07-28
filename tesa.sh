62f552156e0ef9976b00a377e95b8c85eafe64cf
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-07-28 10:17:22 +0700

    Update tesa.sh

--- tesa.sh
+++ tesa.sh
@@ -22,9 +22,9 @@ touch /etc/trojan-go/akun.conf
 mkdir -p /etc/trojan-go/
 chmod 755 /etc/trojan-go/
 touch /etc/trojan-go/trojan-go.pid
-wget -O /usr/local/bin/trojan-go https://github.com/bokir-tampan/ranjau-darate/raw/main/trojan-go
-wget -O /usr/local/bin/geoip.dat https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/geoip.dat
-wget -O /usr/local/bin/geosite.dat https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/geosite.dat
+wget -O /usr/local/bin/trojan-go https://raw.githubusercontent.com/Endka22/Autosc/main/trojan-go
+wget -O /usr/local/bin/geoip.dat https://raw.githubusercontent.com/Endka22/Autosc/main/geoip.dat
+wget -O /usr/local/bin/geosite.dat https://raw.githubusercontent.com/Endka22/Autosc/main/geosite.dat
 chmod +x /usr/local/bin/trojan-go
 
 # Service
