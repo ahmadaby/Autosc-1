1cfcf7c4cdcbf8a6379f9673c94aea0480b867fd
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-07-31 02:29:55 +0700

    Update ins-trojango.sh

--- ins-trojango.sh
+++ ins-trojango.sh
@@ -7,14 +7,13 @@ domain=$(cat /root/domain)
 wget https://raw.githubusercontent.com/Endka22/Autosc/main/bbr.sh &&  chmod +x bbr.sh && ./bbr.sh
 
 # Install Trojan-GO
-	mkdir /etc/trojan-go
-	mkdir /usr/lib/trojan-go
-	wget -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip && unzip -d /usr/lib/trojan-go/ ./trojan-go-linux-amd64.zip && mv /usr/lib/trojan-go/trojan-go /usr/bin/ && chmod +x /usr/bin/trojan-go && rm -rf ./trojan-go-linux-amd64.zip
-	cp /usr/lib/trojan-go/example/server.json /etc/trojan-go/config.json
-	cp /usr/lib/trojan-go/example/trojan-go.service /etc/systemd/system/trojan-go.service
-	systemctl daemon-reload
-	systemctl enable trojan-go
-	echo Done!
+mkdir /etc/trojan-go
+mkdir /usr/lib/trojan-go
+wget -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip && unzip -d /usr/lib/trojan-go/ ./trojan-go-linux-amd64.zip && mv /usr/lib/trojan-go/trojan-go /usr/bin/ && chmod +x /usr/bin/trojan-go && rm -rf ./trojan-go-linux-amd64.zip
+cp /usr/lib/trojan-go/example/server.json /etc/trojan-go/config.json
+cp /usr/lib/trojan-go/example/trojan-go.service /etc/systemd/system/trojan-go.service
+systemctl daemon-reload
+systemctl enable trojan-go
 
 cat <<EOF> /etc/systemd/system/trojan-go.service
 [Unit]
